2bf081fd23a360b8d783700dc87cf52e843d2160
Yasintha <yasinthagunathilake@gmail.com>
2023-04-05 07:13:46 +0530

    Add Task and Task Items Add mock-tasks and Task

---
+++ src/app/Task.ts
@@ -0,0 +1,8 @@
+export interface Task {
+    id? : number;
+    title: string;
+    description: string;
+    addedDate: string;
+    done: boolean;
+    disabled: boolean;
+}
---
+++ src/app/components/tasks/tasks.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Task } from 'src/app/Task';
+import { TASKS } from 'src/app/mock-tasks';
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.css']
+})
+export class TasksComponent {
+
+  tasks: Task[] = TASKS;
+}
